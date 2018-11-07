f2497d5e8f5691b42f9a4f4799c33dff78ba2fef
a <murtazalimtiaz@gmail.com>
2018-11-07 12:26:47 +0500

    commit message

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDy8PobaeiM8GvE0ds-bWkzdw2iDo0CZtQ',
+    authDomain: 'angularexampleapp.firebaseapp.com',
+    databaseURL: 'https://angularexampleapp.firebaseio.com',
+    projectId: 'angularexampleapp',
+    storageBucket: 'angularexampleapp.appspot.com',
+    messagingSenderId: '456717340071'
+  }
+};
